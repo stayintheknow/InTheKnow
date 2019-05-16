7515669644c31fb03c05c31b34310c71b4616cb0
mchanni1321 <Mehak.Channi76@qmail.cuny.edu>
2019-05-16 17:20:40 -0400

    Created layout for Settings Fragment

--- app/src/main/java/stayintheknow/intheknow/fragments/SettingsFragment.java
+++ app/src/main/java/stayintheknow/intheknow/fragments/SettingsFragment.java
@@ -7,13 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import stayintheknow.intheknow.R;
 
 public class SettingsFragment extends Fragment {
+
+    private Button nameBtn;
+    private Button emailBtn;
+    private Button bioBtn;
+    private Button submit_changes;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+
     }
 }
