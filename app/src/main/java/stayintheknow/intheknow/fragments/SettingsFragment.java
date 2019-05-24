2c9d99a0ac711007af752f6a8224452a1bbb353e
iclaude3 <cmbandali3@gmail.com>
2019-05-24 02:12:02 -0400

    updated settings

--- app/src/main/java/stayintheknow/intheknow/fragments/SettingsFragment.java
+++ app/src/main/java/stayintheknow/intheknow/fragments/SettingsFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseUser;
 
@@ -16,13 +17,12 @@ import stayintheknow.intheknow.R;
 
 public class SettingsFragment extends Fragment {
 
-    private Button nameBtn;
-    private Button emailBtn;
-    private Button bioBtn;
+    private Button setting_changes;
     private Button submit_changes;
     private EditText profileName;
     private EditText profileEmail;
     private EditText profileBio;
+    private EditText profileFullName;
 
 
     @Nullable
@@ -35,27 +35,36 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        nameBtn = view.findViewById(R.id.nameBtn);
-        emailBtn = view.findViewById(R.id.emailBtn);
-        bioBtn = view.findViewById(R.id.bioBtn);
+        setting_changes = view.findViewById(R.id.settingBtn);
         submit_changes = view.findViewById(R.id.submit_changes);
         profileName = view.findViewById(R.id.profileName);
         profileEmail = view.findViewById(R.id.profileEmail);
         profileBio = view.findViewById(R.id.profileBio);
+        profileFullName = view.findViewById(R.id.etfullName);
 
 
-        // Retrieve user's current information
-
+        // Retrieve user's current informatio
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
             profileName.setText(currentUser.getUsername());
             profileEmail.setText(currentUser.getEmail());
             profileBio.setText(currentUser.get("bio").toString());
-
+            profileFullName.setText(currentUser.get("name").toString());
         }
 
+        setting_changes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Settings updated", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-
+        submit_changes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Preferences updated", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
